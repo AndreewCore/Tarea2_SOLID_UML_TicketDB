342c55b3ae6f162df3cc0199426361d4cd34be66
Paul Rodriguez <andreewinquires@gmail.com>
2025-06-23 22:56:30 -0500

    Update Notificacion.java

--- tarea2/src/main/java/ec/edu/espol/Notificacion.java
+++ tarea2/src/main/java/ec/edu/espol/Notificacion.java
@@ -1,5 +1,29 @@
 package src.main.java.ec.edu.espol;
 
 public class Notificacion {
-
+    private int id;
+    private String mensaje;
+    private CanalNotificacion canal;
+    private java.time.LocalDateTime fechaEnvio;
+    private boolean leido;
+    
+    public Notificacion(int id, String mensaje, CanalNotificacion canal) {
+        this.id = id;
+        this.mensaje = mensaje;
+        this.canal = canal;
+        this.fechaEnvio = java.time.LocalDateTime.now();
+        this.leido = false;
+    }
+    
+    public void enviarMensaje(String mensaje) {
+        System.out.println("Enviando mensaje: " + mensaje + " por " + canal);
+        this.leido = true;
+    }
+    
+    // Getters y setters
+    public int getId() { return id; }
+    public String getMensaje() { return mensaje; }
+    public CanalNotificacion getCanal() { return canal; }
+    public boolean isLeido() { return leido; }
+    public void setLeido(boolean leido) { this.leido = leido; }
 }
