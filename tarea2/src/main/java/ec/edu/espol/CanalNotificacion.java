4e55a98780f5c98192c386edffeb456c7b869432
Paul Rodriguez <andreewinquires@gmail.com>
2025-06-23 22:54:40 -0500

    Update CanalNotificacion.java

--- tarea2/src/main/java/ec/edu/espol/CanalNotificacion.java
+++ tarea2/src/main/java/ec/edu/espol/CanalNotificacion.java
@@ -1,5 +1,16 @@
 package src.main.java.ec.edu.espol;
 
 public interface CanalNotificacion {
-    
+    void notifBoletoNoDisp();
+    void notifTimeExpired();
+    void notifPagoRealiz();
+    void notifPagoExito();
+    void notifErrorDev();
+    void notifEventoCancel();
+    void notifSoluFail();
+    void notifSoluExito();
+    void notifUsuarioSolu();
+    void notificacionConflicto();
+    void modificacionCanales();
+    void notifError();
 }
